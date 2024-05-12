1a5f98605e8f0a0484209caf078033eb0f8de78b
SWAROOPSAIKAR <143636773+SWAROOPSAIKAR@users.noreply.github.com>
2024-05-12 22:36:16 +0530

    Add files via upload

---
+++ src/mongo.js
@@ -0,0 +1,40 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/LoginForm")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    phonenumber:{
+        type:Number,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,141 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const hbs = require("hbs")
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+
+const tempelatePath = path.join(__dirname, '../templates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+app.use(express.static(publicPath))
+
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/delete', (req, res) => {
+    res.render('delete')
+})
+
+app.get('/update', (req, res) => {
+    res.render('update'); 
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+app.get('/', (req, res) => {
+    res.render('welcome')
+})
+
+
+
+app.post('/signup', async (req, res) => {
+    const data = {
+        name: req.body.name,
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        phonenumber:req.body.phonenumber,
+        email:req.body.email,
+        password: req.body.password
+    }
+
+    try {
+        
+        const checking = await LogInCollection.findOne({ name: req.body.name })
+      
+
+        if (checking) {
+            res.send("Username already exists")
+        } else {
+           
+            await LogInCollection.insertMany([data])
+            res.render("home")
+        }
+    } catch (error) {
+        res.send("An error occurred")
+    }
+})
+
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
+        }
+
+        else {
+            res.send("incorrect password")
+        }
+
+
+    } 
+    
+    catch (e) {
+        res.send("wrong details")    
+
+    }
+
+})
+
+
+app.post('/update', async (req, res) => {
+    try {
+        const { name, newPassword } = req.body;
+
+       
+        const user = await LogInCollection.findOne({ name });
+
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        
+        user.password = newPassword;
+
+        
+        await user.save();
+
+        res.status(200).send("User data updated successfully");
+    } catch (error) {
+        res.status(500).send("An error occurred while updating user data");
+    }
+});
+
+app.post('/delete', async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        const user = await LogInCollection.findOne({ name });
+
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        
+        await LogInCollection.deleteOne({ name });
+
+        res.status(200).send("User data deleted successfully");
+    } catch (error) {
+        res.status(500).send("An error occurred while deleting user data");
+    }
+});
+
+
+app.listen(port, () => {
+    console.log('port connected');
+})
+
+
